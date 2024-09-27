761ed333aff780877496a05f13d8a9fdbeda1091
mahmoud2391997 <mahmoudmelsaid1@gmail.com>
2024-09-27 18:59:14 +0300

    debugingg

--- pages/Checkout/index.tsx
+++ pages/Checkout/index.tsx
@@ -1,6 +1,6 @@
 import axios from "axios";
-import { useRouter } from "next/router";
 import { useState } from "react";
+import { useRouter } from "next/router";
 
 export default function Checkout() {
   const router = useRouter();
