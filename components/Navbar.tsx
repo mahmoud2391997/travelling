58848d0e3a2e19bdc5161e93ff006eda9f724e5e
mahmoud2391997 <mahmoudmelsaid1@gmail.com>
2024-09-28 22:03:08 +0300

    navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -20,27 +20,14 @@ export default function Navbar() {
           height={45}
           className="block md:hidden"
         />
-        <ul className=" justify-between w-[60%] text-white hidden md:flex">
+        <ul className=" justify-around w-[200px] text-white flex">
           <Link href={"/"}>
             <li>Home</li>
           </Link>
-          <li>About</li>
           <Link href={"/tours"}>
             <li>Tours</li>
           </Link>
-          <li>Blog</li>
-          <li>Contact</li>
         </ul>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 448 512"
-          className="w-8 h-8 inline-block md:hidden"
-        >
-          <path
-            fill="white"
-            d="M0 96C0 78.3 14.3 64 32 64l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 128C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 288c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32L32 448c-17.7 0-32-14.3-32-32s14.3-32 32-32l384 0c17.7 0 32 14.3 32 32z"
-          />
-        </svg>{" "}
       </div>
     </div>
   );
